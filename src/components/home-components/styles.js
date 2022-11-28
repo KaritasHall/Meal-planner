390c0f45746e4133e69fe8739e13655d32f42cc8
Karitas <karitashall@gmail.com>
2022-11-28 21:58:59 +0000

    Landing styles

--- src/components/home-components/styles.js
+++ src/components/home-components/styles.js
@@ -1,24 +1,38 @@
+import Lottie from "react-lottie-player";
+import { Link } from "react-router-dom";
 import styled from "styled-components";
 
 export const LandingSection = styled.section`
-  background-color: #fe6532;
-  height: 100vh;
+  background-color: var(--primary-orange);
+  height: 50vh;
   padding-left: 24px;
+  padding-right: 24px;
 `;
 
-export const StartButton = styled.button`
+export const StartButton = styled(Link)`
+  display: grid;
+  align-items: center;
   color: white;
-  background-color: #02195a;
-  padding: 10px;
+  background-color: ${(props) =>
+    props.backgroundColor === "orange"
+      ? "var(--primary-orange)"
+      : "var(--primary-darkblue)"};
   width: 203px;
+  text-align: center;
   height: 64px;
   font-size: 18px;
+  font-weight: bold;
   border-radius: 5px;
+  margin-top: 26px;
+  text-decoration: none;
 `;
+
 export const LandingTitle = styled.h1`
   font-size: 55px;
   font-family: "Lobster", cursive;
   color: white;
+  line-height: 90%;
+  padding-top: 76px;
 `;
 export const LandingSubtitle = styled.h2`
   font-size: 40px;
@@ -26,16 +40,37 @@ export const LandingSubtitle = styled.h2`
   color: white;
   font-style: italic;
   font-weight: 600;
+  margin-top: -8px;
 `;
 
 export const LandingParagraph = styled.p`
   color: white;
   font-size: 20px;
   inline-size: 200px;
+  margin-top: 44px;
+`;
+
+export const FoodAnimation = styled(Lottie)`
+  transition: 300ms;
+  height: 50vh;
+  transform: scale(1.5);
+  @media screen and (max-width: 358px) {
+    transform: scale(1) translateY(40px);
+  }
+`;
+
+export const LottieContainer = styled.div`
+  display: flex;
+  align-items: center;
+  width: 100%;
+  display: grid;
+  align-items: center;
+  background-color: var(--primary-orange);
+  overflow: hidden;
 `;
 
 export const AboutSection = styled.section`
-  background-color: #b0dafe;
+  background-color: var(--primary-lightblue);
   height: 100vh;
 `;
 
@@ -43,4 +78,26 @@ export const AboutTitle = styled.h2`
   font-size: 40px;
   font-family: "Barlow";
   color: var(--primary-darkblue);
+  margin: 0;
+  text-align: center;
+  padding-top: 98px;
+`;
+
+export const AboutContainer = styled.div`
+  padding-top: 50px;
+`;
+
+export const AboutDescription = styled.p`
+  color: var(--primary-darkblue);
+  font-size: 20px;
+`;
+
+export const AboutContent1 = styled.div`
+  display: flex;
+`;
+export const AboutContent2 = styled.div`
+  display: flex;
+`;
+export const AboutContent3 = styled.div`
+  display: flex;
 `;
--- src/pages/home.js
+++ src/pages/home.js
@@ -1,13 +1,25 @@
 import React from "react";
-import Button from "../components/home-components/button";
 import Header from "../components/header/header";
+import chefhat from "../components/home-components/icons/chefhat.svg";
+import calendar from "../components/home-components/icons/calendar.svg";
+import shopping from "../components/home-components/icons/shopping.svg";
+import lottieJson from "../components/home-components/icons/lottie-anim.json";
+
 import {
+  FoodAnimation,
   LandingTitle,
   LandingSubtitle,
   LandingParagraph,
   LandingSection,
   AboutSection,
   AboutTitle,
+  AboutContainer,
+  AboutContent1,
+  AboutContent2,
+  AboutContent3,
+  AboutDescription,
+  LottieContainer,
+  StartButton,
 } from "../components/home-components/styles";
 
 function Home() {
@@ -20,10 +32,37 @@ function Home() {
         <LandingParagraph>
           Plan your whole week with just a few clicks!
         </LandingParagraph>
-        <Button />
+        <StartButton to="/weekly-overview">Start Planning</StartButton>
       </LandingSection>
+      <LottieContainer>
+        <FoodAnimation loop animationData={lottieJson} play />
+      </LottieContainer>
       <AboutSection>
         <AboutTitle>Get Started</AboutTitle>
+        <AboutContainer>
+          <AboutContent1>
+            <AboutDescription>
+              Browse quickly through our recipes and discover dinner ideas with
+              ease
+            </AboutDescription>
+            <img src={chefhat} />
+          </AboutContent1>
+          <AboutContent2>
+            <img src={calendar} />
+            <AboutDescription>
+              Pick and add a recipe for every day of the week
+            </AboutDescription>
+          </AboutContent2>
+          <AboutContent3>
+            <AboutDescription>
+              Grab your ready-made shopping list and go shopping!
+            </AboutDescription>
+            <img src={shopping} />
+          </AboutContent3>
+        </AboutContainer>
+        <StartButton to="/weekly-overview" backgroundColor="orange">
+          Start Planning
+        </StartButton>
       </AboutSection>
     </>
   );
