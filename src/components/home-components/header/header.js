748acab3394a4919335fdf9a3c00ad6efcbb9a68
Karitas <karitashall@gmail.com>
2022-12-01 10:29:36 +0000

    logo on landing page

---
+++ src/components/home-components/header/header.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { HeaderContainer } from "./styles";
+import logo from "./LandingLogo.svg";
+
+export default function Header() {
+  return (
+    <>
+      <HeaderContainer className="with-gutter" src={logo} />
+    </>
+  );
+}
